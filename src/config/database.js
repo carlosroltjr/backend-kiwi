be9029d266d6286a3a49677070b4a7587d71d920
carlosroltjr <32556410+carlosroltjr@users.noreply.github.com>
2020-12-02 22:55:59 -0300

    Update database.js

--- src/config/database.js
+++ src/config/database.js
@@ -1,9 +1,9 @@
 module.exports = {
   dialect: 'postgres',
-  host: 'ec2-52-203-165-126.compute-1.amazonaws.com',
-  username: 'odumuqyvsemanu',
-  password: 'fd1eb9816b746061fcf6db33fbe49db718442bf4d0a468cb089535f5e26ac71d',
-  database: 'da5a3791redn70',
+  host: 'ec2-23-20-205-19.compute-1.amazonaws.com',
+  username: 'csykuvwtakyxdg',
+  password: 'eb12dd5ec0f12513d96197eb0882159ab9e59be01d92b77746709d2ead285245',
+  database: 'd820srra921n31',
   define: {
     timestamp: true
   }
