0aec8bf35ef074319e3286d7dcf39f0e8ae8001e
carlosroltjr <32556410+carlosroltjr@users.noreply.github.com>
2020-12-03 12:09:28 -0300

    usando variáveis de ambiente

--- src/config/database.js
+++ src/config/database.js
@@ -1,9 +1,9 @@
 module.exports = {
   dialect: 'postgres',
-  host: 'ec2-23-20-205-19.compute-1.amazonaws.com',
-  username: 'csykuvwtakyxdg',
-  password: 'eb12dd5ec0f12513d96197eb0882159ab9e59be01d92b77746709d2ead285245',
-  database: 'd820srra921n31',
+  host: process.env.DATABASE_HOST || 'localhost',
+  username: process.env.DATABASE_USERNAME || 'postgres',
+  password: process.env.DATABASE_PASSWORD || 'docker',
+  database: process.env.DATABASE || 'sa-2020',
   define: {
     timestamp: true
   }
